d9d47e0e539fc639684a02e240e7a066ab2af814
m <914759402@qq.com>
2024-07-09 11:28:57 +0800

    添加注释

--- src/main/java/org/example/AddressBookDynamicDeserializerpPro.java
+++ src/main/java/org/example/AddressBookDynamicDeserializerpPro.java
@@ -13,30 +13,42 @@ import java.util.Map;
  */
 public class AddressBookDynamicDeserializerpPro {
     public static void main(String[] args) throws Exception {
-        // protobuf序列化后的byte[]
+        // 生成并序列化 AddressBook 消息
         Addressbook.AddressBook addressBook = initMsg();
 
+        // 从文件 "./src/main/proto/cinema1.description" 中解析 FileDescriptorSet 对象
         DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet
                 .parseFrom(new FileInputStream("./src/main/proto/cinema1.description"));
 
+        // 创建 SelfDescribingMessage 的构建器
         Selfmd.SelfDescribingMessage.Builder selfmdBuilder = Selfmd.SelfDescribingMessage.newBuilder();
+        // 设置描述符集合
         selfmdBuilder.setDescriptorSet(descriptorSet);
+        // 设置消息名称
         selfmdBuilder.setMsgName(Addressbook.AddressBook.getDescriptor().getFullName());
+        // 设置消息内容（序列化的字节数组）
         selfmdBuilder.setMessage(addressBook.toByteString());
+        // 构建 SelfDescribingMessage 并序列化为字节数组
         byte[] byteArray = selfmdBuilder.build().toByteArray();
 
+        // 从字节数组中反序列化 SelfDescribingMessage 对象
         Selfmd.SelfDescribingMessage parseFrom = Selfmd.SelfDescribingMessage.parseFrom(byteArray);
+        // 获取描述符集合
         DescriptorProtos.FileDescriptorSet descriptorSet2 = parseFrom.getDescriptorSet();
+        // 获取消息内容（字节串）
         ByteString message = parseFrom.getMessage();
+        // 获取消息名称
         String msgName = parseFrom.getMsgName();
 
         Descriptors.Descriptor pbDescritpor = null;
-        for (DescriptorProtos.FileDescriptorProto fdp : descriptorSet2
-                .getFileList()) {
+        // 遍历每一个 FileDescriptorProto 对象
+        for (DescriptorProtos.FileDescriptorProto fdp : descriptorSet2.getFileList()) {
+            // 构建 FileDescriptor 对象
             Descriptors.FileDescriptor fileDescriptor = Descriptors.FileDescriptor
                     .buildFrom(fdp, new Descriptors.FileDescriptor[] {});
-            for (Descriptors.Descriptor descriptor : fileDescriptor
-                    .getMessageTypes()) {
+            // 遍历每一个消息类型（Descriptor 对象）
+            for (Descriptors.Descriptor descriptor : fileDescriptor.getMessageTypes()) {
+                // 查找名称与 msgName 匹配的消息类型
                 if (descriptor.getName().equals(msgName)) {
                     System.out.println("descriptor found");
                     pbDescritpor = descriptor;
@@ -45,10 +57,13 @@ public class AddressBookDynamicDeserializerpPro {
             }
         }
 
+        // 如果没有找到匹配的描述符，打印错误信息并返回
         if (pbDescritpor == null) {
             System.out.println("No matched descriptor");
             return;
         }
+
+        // 使用找到的描述符解析消息内容
         DynamicMessage pbMessage = DynamicMessage.parseFrom(pbDescritpor, message);
         // 打印消息内容
         printMessage(pbMessage);
@@ -60,18 +75,18 @@ public class AddressBookDynamicDeserializerpPro {
             Descriptors.FieldDescriptor field = entry.getKey();
             Object value = entry.getValue();
 
+            // 处理重复字段
             if (field.isRepeated()) {
-                // 处理重复字段
                 List<?> values = (List<?>) value;
                 for (Object item : values) {
+                    // 如果值是消息类型，递归调用 printMessage 方法
                     if (item instanceof Message) {
-                        // 如果值是消息类型，递归调用 printMessage 方法
                         printMessage((Message) item);
                     }
                 }
             } else {
+                // 如果值是消息类型，递归调用 printMessage 方法
                 if (value instanceof Message) {
-                    // 如果值是消息类型，递归调用 printMessage 方法
                     printMessage((Message) value);
                 } else {
                     // 打印字段的 JSON 名称和字段值
@@ -80,7 +95,6 @@ public class AddressBookDynamicDeserializerpPro {
             }
         }
     }
-
 
     private static Addressbook.AddressBook initMsg() {
         // 创建 AddressBook 的构建器
@@ -97,10 +111,8 @@ public class AddressBookDynamicDeserializerpPro {
         // 将人员信息添加到地址簿
         addressBook.addPeople(person1);
 
-        // 将地址簿构建成字节数组（序列化）
-        Addressbook.AddressBook build = addressBook.build();
-
-        return build;
+        // 构建并返回 AddressBook 对象
+        return addressBook.build();
     }
-
 }
+
